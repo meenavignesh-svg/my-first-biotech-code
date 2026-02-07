0458b0ab0a6a0e3bfdad566e13f77100e6b32d7d
meenavignesh-svg <meenavignesh6789@gmail.com>
2026-02-07 19:01:43 +0530

    Update hello_dna.py

--- hello_dna.py
+++ hello_dna.py
@@ -1,3 +1,7 @@
-dna = "ATGC"
-print("My DNA sequence is:", dna)
-print("It has", len(dna), "letters!")
+dna_recipe = "ATGTTCGGT"
+rna_copy = dna_recipe.replace("T", "U")
+protein = "MFG" 
+
+print("Step 1 (DNA):", dna_recipe)
+print("Step 2 (RNA):", rna_copy)
+print("Step 3 (Protein Cake):", protein)
